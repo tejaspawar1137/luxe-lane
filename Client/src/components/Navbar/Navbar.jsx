f57bafcadccaaf68f8810aa0bf4bcd8cf39d6299
Dhruv Kumar Sharma <115400138+Dhruv628@users.noreply.github.com>
2024-02-04 23:23:05 +0530

    Update Navbar.jsx

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
   const authToken = useSelector((state) => state.UserReducer.authToken);
 
   const fetchUser = async () => {
-    const apiUrl = "/api/user/getUser";
+    const apiUrl = "https://luxe-lane-backend.vercel.app/api/user/getUser";
     const headers = { "content-type": "application/json", token: authToken };
     try {
       setCartMenuLoading(true);
